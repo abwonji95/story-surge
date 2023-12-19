06d0beafbb7bcc9f26d32dfb0d54c9b4b32bd26c
abwonji95 <abwonji95@gmail.com>
2023-12-19 22:02:10 +0300

    horrendous

---
+++ src/app/components/carousel/page.tsx
@@ -0,0 +1,13 @@
+import { Carousel } from "react-responsive-carousel"
+import 'react-responsive-carousel/lib/styles/carousel.css'
+
+export default function CarouselFlow(){
+    return (
+        <div>
+            <Carousel showThumbs={false} autoPlay>
+
+
+            </Carousel>
+        </div>
+    )
+}
